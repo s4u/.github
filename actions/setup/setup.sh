1fdbcf4eca6f7daf8eee2246504fa5312ad6c96e
Slawomir Jaranowski <s.jaranowski@gmail.com>
2022-11-13 10:09:00 +0100

    Replace deprecate GH set-output command

--- actions/setup/setup.sh
+++ actions/setup/setup.sh
@@ -3,58 +3,22 @@
 echo "::group::Setup preparing"
 
 SONAR=$(echo "MmRlMGUzMmIyYjMxMDg5NjVmNzZhNDcxM2JhMjAyODk3OGMyMzNlZAo=" | openssl base64 -d)
+echo "sonar=${SONAR}" >> $GITHUB_OUTPUT
 
-echo "::set-output name=sonar::${SONAR}"
-
-if [ -z "${DATE_YM}" ]; then
-  DATE_YM=$(date +%y%m)
-fi
-
-echo "::set-output name=date-ym::${DATE_YM}"
+DATE_YM=$(date +%y%m)
+echo "date-ym=${DATE_YM}" >> $GITHUB_OUTPUT
 echo "date-ym=${DATE_YM}"
 
 if [ -z "${GITHUB_REF##*heads*}" ]; then
   BRANCH=${GITHUB_REF#refs/heads/}
-  echo "::set-output name=branch::${BRANCH}"
+  echo "branch=${BRANCH}" >> $GITHUB_OUTPUT
   echo "branch=${BRANCH}"
 fi
 
 if [ -z "${GITHUB_REF##*tags*}" ]; then
   TAG=${GITHUB_REF#refs/tags/}
-  echo "::set-output name=tag::${TAG}"
+  echo "tag=${TAG}" >> $GITHUB_OUTPUT
   echo "tag=${TAG}"
 fi
-
-# java versions
-
-if [ -z "${JAVA_DEFAULT}" ]; then
-  JAVA_DEFAULT='8'
-fi
-
-echo "::set-output name=java-default::${JAVA_DEFAULT}"
-echo "java-default=${JAVA_DEFAULT}"
-
-if [ -z "$JAVA_MATRIX" ]; then
-  JAVA_MATRIX='"8", "11", "17"'
-fi
-
-echo "::set-output name=java-matrix::[${JAVA_MATRIX}]"
-echo "java-matrix=[${JAVA_MATRIX}]"
-
-# java distribution
-
-if [ -z "${JAVA_DISTRIBUTION_DEFAULT}" ]; then
-  JAVA_DISTRIBUTION_DEFAULT='zulu'
-fi
-
-echo "::set-output name=java-distribution-default::${JAVA_DISTRIBUTION_DEFAULT}"
-echo "java-distribution-default=${JAVA_DISTRIBUTION_DEFAULT}"
-
-if [ -z "$JAVA_DISTRIBUTION_MATRIX" ]; then
-  JAVA_DISTRIBUTION_MATRIX='"zulu"'
-fi
-
-echo "::set-output name=java-distribution-matrix::[${JAVA_DISTRIBUTION_MATRIX}]"
-echo "java-distribution-matrix=[${JAVA_DISTRIBUTION_MATRIX}]"
 
 echo "::endgroup::"
